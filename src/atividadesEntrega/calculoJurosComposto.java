e4601a82d0e8e18442989008d48b27517a9ac13f
PedroEmanuel-22 <emanuelpedrossp@gmail.com>
2025-02-21 14:55:42 -0300

    Initial commit

---
+++ src/atividadesEntrega/calculoJurosComposto.java
@@ -0,0 +1,10 @@
+package atividadesEntrega;
+
+public class calculoJurosComposto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/atividadesEntrega/calculoMediaAluno.java
@@ -0,0 +1,10 @@
+package atividadesEntrega;
+
+public class calculoMediaAluno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
